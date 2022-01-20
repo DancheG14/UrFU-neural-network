ffa9c40b0215ac738a2ff03682045227a358484b
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-01-20 17:55:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ class Item(BaseModel):
 
 app = FastAPI()
 
-classifier = pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")
+classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
 
 print(classifier("Привет! Как дела?"))
 print(classifier("Привет! Я убью тебя лодочнин!"))
